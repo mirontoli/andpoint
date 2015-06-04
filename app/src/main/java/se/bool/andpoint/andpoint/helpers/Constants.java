451c7d471d79fd45e63a22d41ecde11101cb30af
Anatoly Mironov <mirontoli@gmail.com>
2015-06-04 12:58:08 +0200

    Constants

---
+++ app/src/main/java/se/bool/andpoint/andpoint/helpers/Constants.java
@@ -0,0 +1,22 @@
+package se.bool.andpoint.andpoint.helpers;
+
+/**
+ * Created by anatoly on 2015-06-04.
+ */
+public interface Constants {
+    public static final String AUTHORITY_URL = "https://login.windows.net/common";
+    public static final String DISCOVERY_RESOURCE_URL = "https://api.office.com/discovery/v1.0/me/";
+    public static final String DISCOVERY_RESOURCE_ID = "https://api.office.com/discovery/";
+    public static final String MYFILES_CAPABILITY = "MyFiles";
+    public static final String CALENDAR_CAPABILITY = "Calendar";
+    public static final String MAIL_CAPABILITY = "Mail";
+    public static final String CALENDER_ID = "Calendar";
+    // Update these two constants with the values for your application:
+    public static final String CLIENT_ID = "18771ad2-1bbf-49c3-b597-227c32cdad1a";
+    public static final String REDIRECT_URI = "https://andpoint.bool.se";
+    public static final String MAIL_OPERATION_ADD = "Add";
+    public static final String MAIL_OPERATION_DELETE = "Delete";
+    public static final String MAIL_OPERATION_RESULT_SUCCESS = "Added";
+    public static final String MAIL_OPERATION_RESULT_FAIL = "Failed";
+    public static final String MAIL_DETAIL_FRAGMENT_TAG = "mail_detail_fragment_tag";
+}
