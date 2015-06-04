8c109353e50100609a66d62f6199ef1d27ad92b9
Dan <rlv@rlvision.com>
2015-06-04 10:52:00 +0200

    Added button and text field

--- app/src/main/java/se/bool/andpoint/andpoint/MainActivity.java
+++ app/src/main/java/se/bool/andpoint/andpoint/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupButton();
     }
 
     @Override
@@ -35,4 +41,21 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void setupButton() {
+        View.OnClickListener btnClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Getting data...", Toast.LENGTH_LONG).show();
+
+                TextView txt = (TextView)findViewById(R.id.textViewOutput);
+                txt.setText("Hello\nBool");
+            }
+        };
+
+        Button btn = (Button)findViewById(R.id.buttonGetData);
+        btn.setOnClickListener(btnClick);
+    }
+
+
 }
